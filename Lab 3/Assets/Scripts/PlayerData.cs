b1685c6d675cf05b970eed9305f468010cd89a6a
varant-t <vv_titizian@hotmail.ca>
2020-08-06 17:07:03 -0400

    Lab 3

---
+++ Lab 3/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerData : MonoBehaviour
+{
+   
+
+
+    public Hashtable playerData;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        /*playerData = new Hashtable();
+
+        playerData.Add(0, "Varant");
+        playerData.Add(1, "Charles");
+        playerData.Add(2, "Matt");
+        playerData.Add(3, "Craig");
+
+        /*
+        playerData.Add("Varant", new PlayerDataEntry { Name = "Varant", Level = Random.Range(1, 100), Score = Random.Range(1, 1000) });
+        playerData.Add("Matt", new PlayerDataEntry { Name = "Matt", Level = Random.Range(1, 100), Score = Random.Range(1, 1000) });
+        playerData.Add("Charles", new PlayerDataEntry { Name = "Charles", Level = Random.Range(1, 100), Score = Random.Range(1, 1000) });
+        playerData.Add("Craig", new PlayerDataEntry { Name = "Craig", Level = Random.Range(1, 100), Score = Random.Range(1, 1000) });
+        */
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void CheckEntry()
+    {
+        
+    }
+}
---
+++ Lab 3/Assets/Scripts/PlayerDataEntry.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerDataEntry : MonoBehaviour
+{
+    public string Name;
+    public int Level;
+    public int Score;
+    public Hashtable playerData;
+    public PlayerData pd;
+
+    public TextMeshPro TextPlayerName;
+    public TextMeshPro TextPlayerLevel;
+    public TextMeshPro TextPlayerScore;
+    public TMP_InputField InputField;
+
+    public TextMeshProUGUI displayText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextPlayerName = GameObject.FindObjectOfType<TextMeshPro>();
+
+        InputField = GameObject.FindObjectOfType<TMP_InputField>();
+
+        playerData = new Hashtable();
+
+        playerData.Add(0, "Varant");
+        playerData.Add(1, "Charles");
+        playerData.Add(2, "Matt");
+        playerData.Add(3, "Craig");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void CallingEntry()
+    {
+        if(playerData.ContainsValue(InputField.text))
+        {
+
+            Debug.Log(InputField.text);
+        }
+        Debug.Log(InputField.text);
+
+    }    
+
+    void varantStats()
+    {
+        Name = "Varant";
+        Level = 100;
+        Score = 1000;
+    }
+
+    void chalresStats()
+    {
+        Name = "Charles";
+        Level = 80;
+        Score = 800;
+    }
+
+    void mattStats()
+    {
+        Name = "Matt";
+        Level = 60;
+        Score = 600;
+    }
+
+    void craigStats()
+    {
+        Name = "Craig";
+        Level = 40;
+        Score = 400;
+    }
+}
+
+
+
